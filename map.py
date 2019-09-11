92c616017f7c5e7f312ea52bbf14dad8953f6f85
Rahul Garg <vihu89@gmail.com>
2019-09-11 11:02:46 -0700

    Change module doc

--- map.py
+++ map.py
@@ -1,17 +1,5 @@
-""" flask_example.py
-
-    Required packages:
-    - flask
-    - folium
-
-    Usage:
-
-    Start the flask server by running:
-
-        $ python flask_example.py
-
-    And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
-
+"""
+Plot hotspot poc related information on map
 """
 
 import folium
